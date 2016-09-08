f1ded4b5931bc5f5798db14cef91e88f1d7a5f56
vjeeva <vjeeva@uwaterloo.ca>
2016-09-07 21:31:35 -0400

    Working copy

--- Arduino/AmpControl/AmpControl.ino
+++ Arduino/AmpControl/AmpControl.ino
@@ -4,27 +4,34 @@
 Servo myservo;
 int bluetoothTx = 10;
 int bluetoothRx = 11;
+bool ampOn = true;
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
 {
   myservo.attach(9);
-  Serial.begin(115200);
+  Serial.begin(9600);
   bluetooth.begin(9600);
+  pinMode(2, OUTPUT);
 }
 
 void loop()
 {
-  Serial.println("Hi");
-  if (Serial.available() > 0){
-    //Serial.println("Hi");
-    //myservo.write(Serial.read());
-  }
   if(bluetooth.available()> 0 )
   {
-    //int servopos = bluetooth.read();
-    //Serial.println(servopos);
-    //myservo.write(servopos);
-  
+    byte servopos = bluetooth.read();
+    Serial.write((int)servopos);
+    if (((int) servopos) == 255){
+      ampOn = !ampOn;
+      if (ampOn){
+        digitalWrite(2,1);
+      } else {
+        digitalWrite(2,0);
+      }
+    } else {
+      myservo.write((int)servopos);
+      Serial.write(" ");
+    }
+    delay(10);
   }
 }
